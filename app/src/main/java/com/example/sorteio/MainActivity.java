2a95eaaac27d606c02da8ed0b89283d7791bb5ab
Devsoares-prog <58860571+Devsoares-prog@users.noreply.github.com>
2020-03-18 15:20:01 -0300

    começo do project

---
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sortearNumero(View view){
+        TextView texto = findViewById(R.id.textoResultado);
+        int x = new Random().nextInt(11);
+        texto.setText("       O Número Sorteado foi:" + x);
+    }
+
+
+
+
+
+
+}
